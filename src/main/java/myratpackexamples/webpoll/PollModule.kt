95c4c8b27f076db8d98fc3b9bdecdd4d740a1ce2
tkreyling <thomas.kreyling@googlemail.com>
2017-10-27 21:09:18 +0200

    Introduced Kotlin

---
+++ src/main/java/myratpackexamples/webpoll/PollModule.kt
@@ -0,0 +1,11 @@
+package myratpackexamples.webpoll
+
+import com.google.inject.Binder
+import com.google.inject.Module
+
+class PollModule : Module {
+    override fun configure(binder: Binder) {
+        binder.bind(CreatePollHandler::class.java)
+        binder.bind(RetrievePollHandler::class.java)
+    }
+}
