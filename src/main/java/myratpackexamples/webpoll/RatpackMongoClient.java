c0b15726b0bb7266c31dfab50b3a1c9de04a89b1
tkreyling <thomas.kreyling@googlemail.com>
2017-10-27 07:38:14 +0200

    Renamed Error to FindOneError to reflect that these are only the errors of findOne

--- src/main/java/myratpackexamples/webpoll/RatpackMongoClient.java
+++ src/main/java/myratpackexamples/webpoll/RatpackMongoClient.java
@@ -2,7 +2,6 @@ package myratpackexamples.webpoll;
 
 import com.mongodb.async.client.MongoCollection;
 import io.vavr.control.Validation;
-import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -16,18 +15,6 @@ import static ratpack.exec.Promise.async;
 
 public class RatpackMongoClient {
 
-    public abstract static class Error {
-    }
-
-    @Value
-    public static class ExactlyOneElementExpected extends Error {
-    }
-
-    @Value
-    public static class MongoException extends Error {
-        Throwable throwable;
-    }
-
     public static Promise<Void> insertOne(MongoCollection<Document> collection, Document document) {
         return async(downstream -> collection.insertOne(document, (result, throwable) -> {
             if (throwable != null) {
@@ -38,10 +25,22 @@ public class RatpackMongoClient {
         }));
     }
 
-    public static Promise<Validation<Error, Document>> findOne(MongoCollection<Document> collection, Bson filter) {
+    public abstract static class FindOneError {
+    }
+
+    @Value
+    public static class ExactlyOneElementExpected extends FindOneError {
+    }
+
+    @Value
+    public static class MongoError extends FindOneError {
+        Throwable throwable;
+    }
+
+    public static Promise<Validation<FindOneError, Document>> findOne(MongoCollection<Document> collection, Bson filter) {
         return async(downstream -> collection.find(filter).into(new ArrayList<>(), (result, throwable) -> {
             if (throwable != null) {
-                downstream.success(invalid(new MongoException(throwable)));
+                downstream.success(invalid(new MongoError(throwable)));
             } else {
                 if (result.size() != 1) {
                     downstream.success(invalid(new ExactlyOneElementExpected()));
