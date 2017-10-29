8386050609079566052ff3ad8a267e375f3510d7
tkreyling <thomas.kreyling@googlemail.com>
2017-10-29 09:01:34 +0100

    Renamed file RatpackMongoClient to MongoCollectionExtensions

--- src/test/java/myratpackexamples/webpoll/MongoTest.java
+++ src/test/java/myratpackexamples/webpoll/MongoTest.java
@@ -14,7 +14,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.Arrays.asList;
-import static myratpackexamples.webpoll.RatpackMongoClientKt.insertOne;
+import static myratpackexamples.webpoll.MongoCollectionExtensionsKt.insertOne;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
