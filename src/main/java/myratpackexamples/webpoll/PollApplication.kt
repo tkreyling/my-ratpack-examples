d9b3dfeeb698d4cfe68fe0c820ad9db7647b1a07
tkreyling <thomas.kreyling@googlemail.com>
2017-10-29 10:54:47 +0100

    Replaced block by expression body

--- src/main/java/myratpackexamples/webpoll/PollApplication.kt
+++ src/main/java/myratpackexamples/webpoll/PollApplication.kt
@@ -5,17 +5,13 @@ import ratpack.handling.Chain
 import ratpack.server.RatpackServer
 import ratpack.server.RatpackServerSpec
 
-private fun addHandlersToChain(chain: Chain): Chain {
-    return chain
-            .post("poll", CreatePollHandler::class.java)
-            .get("poll/:poll", RetrievePollHandler::class.java)
-}
+private fun addHandlersToChain(chain: Chain): Chain = chain
+        .post("poll", CreatePollHandler::class.java)
+        .get("poll/:poll", RetrievePollHandler::class.java)
 
-fun setupServer(server: RatpackServerSpec): RatpackServerSpec {
-    return server
-            .registry(Guice.registry { b -> b.module(PollModule::class.java) })
-            .handlers { addHandlersToChain(it) }
-}
+fun setupServer(server: RatpackServerSpec): RatpackServerSpec = server
+        .registry(Guice.registry { it.module(PollModule::class.java) })
+        .handlers { addHandlersToChain(it) }
 
 fun main(args: Array<String>) {
     InMemoryMongoDb()
