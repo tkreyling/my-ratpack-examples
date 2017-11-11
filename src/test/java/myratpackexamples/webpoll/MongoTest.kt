5a7436a30e31ee070a372422a389f6f8cf43ffc2
tkreyling <thomas.kreyling@googlemail.com>
2017-11-11 22:25:16 +0100

    Extracted methods createConnectionAndGetCollection and createPollDocument

--- src/test/java/myratpackexamples/webpoll/MongoTest.kt
+++ src/test/java/myratpackexamples/webpoll/MongoTest.kt
@@ -2,6 +2,7 @@ package myratpackexamples.webpoll
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.mongodb.async.client.MongoClients
+import com.mongodb.async.client.MongoCollection
 import org.bson.Document
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
@@ -10,8 +11,6 @@ import ratpack.exec.Operation
 import ratpack.test.exec.ExecHarness
 import java.util.Arrays.asList
 import java.util.Collections.emptyList
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 
 class MongoTest {
     private val objectMapper = ObjectMapper()
@@ -20,34 +19,31 @@ class MongoTest {
     internal fun testDb() {
         ExecHarness.executeSingle(Operation.of {
             InMemoryMongoDb().use { inMemoryMongoDb ->
-
-                val mongo = MongoClients.create(inMemoryMongoDb.connectionString)
-                val database = mongo.getDatabase("test")
-
-                val latch = CountDownLatch(1)
-
-                val collection = database.getCollection("test")
-
-                val poll = PollResponse.Poll(
-                        null, "Sport to play on Friday", asList("basketball"), emptyList()
-                )
-                val pollJson = objectMapper.writeValueAsString(poll)
-
-                val doc = Document.parse(pollJson)
-
-                assertEquals("Sport to play on Friday", doc.getString("topic"))
-                assertEquals(asList("basketball"), doc["options"])
+                val collection = createConnectionAndGetCollection(inMemoryMongoDb)
+                val doc = createPollDocument()
 
                 collection.insertOne(doc)
-                        .result { voidExecResult ->
-                            println("inserted!")
-                            latch.countDown()
-
-                            assertNotNull(doc["_id"])
-                        }
-
-                latch.await(2, TimeUnit.SECONDS)
+                        .result { assertNotNull(doc["_id"]) }
             }
         })
+    }
+
+    private fun createPollDocument(): Document {
+        val poll = PollResponse.Poll(
+                null, "Sport to play on Friday", asList("basketball"), emptyList()
+        )
+        val pollJson = objectMapper.writeValueAsString(poll)
+        val doc = Document.parse(pollJson)
+
+        assertEquals("Sport to play on Friday", doc.getString("topic"))
+        assertEquals(asList("basketball"), doc["options"])
+
+        return doc
+    }
+
+    private fun createConnectionAndGetCollection(inMemoryMongoDb: InMemoryMongoDb): MongoCollection<Document> {
+        val mongo = MongoClients.create(inMemoryMongoDb.connectionString)
+        val database = mongo.getDatabase("test")
+        return database.getCollection("test")
     }
 }
